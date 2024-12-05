c5a924c2b6ed23d3aacea21333bebd72a46ec91a
tszub <bela.szuber@gmail.com>
2024-12-05 14:38:54 +0100

    helloworld flask

--- batselector.py
+++ batselector.py
@@ -1,5 +1,20 @@
 import os
 import csv
-port = int(os.environ.get('PORT', '0.0.0.0:8080'))
-with open('Dataset1.csv', newline='')as csvfile:
-  batreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World! Welcome to your Python web app."
+
+@app.route("/about")
+def about():
+    return "This is the about page."
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#port = int(os.environ.get('PORT', '0.0.0.0:8080'))
+#with open('Dataset1.csv', newline='')as csvfile:
+ # batreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
