c5657a25323ffc918078d9840dc09758c196a764
talszuber <106447224+talszuber@users.noreply.github.com>
2024-12-05 13:15:26 +0100

    Update batselector.py

--- batselector.py
+++ batselector.py
@@ -1,4 +1,5 @@
-const port = process.env.PORT || 4000
+import os
 import csv
+port = int(os.environ.get('PORT', 1000))
 with open('Dataset1.csv', newline='')as csvfile:
   batreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
