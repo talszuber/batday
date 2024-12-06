bd7f949ef81c444de32b23a018ce417b7cfc631e
tszub <bela.szuber@gmail.com>
2024-12-06 13:44:15 +0100

    wsgi config file

--- batselector.py
+++ batselector.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-@oidc.accept_token(True)
 def home():
     return "Hello, World! Welcome to your Python web app."
 
