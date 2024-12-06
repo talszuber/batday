39acebbc3ef1a4a5b772308678f7989cea6faccf
tszub <bela.szuber@gmail.com>
2024-12-06 13:59:17 +0100

    start_response added

--- batselector.py
+++ batselector.py
@@ -4,15 +4,13 @@ import uvicorn
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+start_response('200 OK', [('Content-Type', 'text/plain')])
 @app.route("/home", methods=["GET", "POST"])
 def home():
-    start_response('200 OK', [('Content-Type', 'text/plain')])
     return "Hello, World! Welcome to your Python web app."
 
 @app.route("/about")
 def about():
-    start_response('200 OK', [('Content-Type', 'text/plain')])
     return "This is your daily bat selection website."
 
 
